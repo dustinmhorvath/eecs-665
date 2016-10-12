3c4f40fe361d87ded4c22beaed43b65528d3ad55
Dustin Horvath <dustinmhorvath@gmail.com>
2016-10-12 13:01:59 -0500

    Moving along.

--- project1/main.cpp
+++ project1/main.cpp
@@ -5,8 +5,48 @@
 #include <cctype>
 #include <sstream>
 #include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
+
+vector<int> epsilonClosureHelper(std::vector<int> states[][27], int row, int epsilonCol){
+  vector<int> closure;
+  for(int i = 0; i < states[row][epsilonCol].size(); i++){
+    if(!states[row][epsilonCol].empty()){
+      closure.push_back(states[row][epsilonCol].at(i));
+      vector<int> recurseClose = epsilonClosureHelper(states, states[row][epsilonCol].at(i), epsilonCol);
+      closure.insert(closure.end(), recurseClose.begin(), recurseClose.end());
+    }
+  }
+  return closure;
+}
+
+vector<int> epsilonClosure(std::vector<int> states[][27], int row, int epsilonCol){
+  vector<int> closure;
+  closure.push_back(row);
+  for(int i = 0; i < states[row][epsilonCol].size(); i++){
+    if(!states[row][epsilonCol].empty()){
+      closure.push_back(states[row][epsilonCol].at(i));
+      vector<int> recurseClose = epsilonClosureHelper(states, states[row][epsilonCol].at(i), epsilonCol);
+      closure.insert(closure.end(), recurseClose.begin(), recurseClose.end());
+    }
+  }
+  return closure;
+}
+
+vector<int> navigate(std::vector<int> states[][27], vector<int> tocheck, int col){
+  vector<int> destinations;
+  for(int vectorindex = 0; vectorindex < tocheck.size(); vectorindex++){
+    for(int i = 0; i < states[vectorindex][col].size(); i++){
+      if(!states[vectorindex][col].empty()){
+        destinations.push_back(states[vectorindex][col].at(i));
+      }
+    }
+  }
+  return destinations;
+}
+
 
 
 int main(){
@@ -35,15 +75,19 @@ int main(){
   std::getline(infile, line);
 
   // Assuming we don't use more than the alphabet's worth of states, plus E
-  std::vector<int> states[totalstates][27];
+  std::vector<int> NFAstates[totalstates][27];
+  std::vector<int> DFAstates[totalstates][27];
 
+  // Parse table of inputs
   std::string word;
   int row = 0;
+  int lastcolumn = 0;
   while (std::getline(infile, line)){
     std::stringstream ss(line);
     // Consume row number
     ss >> word;
 
+    // Tokenize states
     int column = 0;
     while(ss >> word){
       first = word.find('{');
@@ -52,7 +96,7 @@ int main(){
       std::stringstream commaparse(nocurlies);
       int i;
       while (commaparse >> i){
-        states[row][column].push_back(i);
+        NFAstates[row][column].push_back(i);
 
         if (commaparse.peek() == ',')
           commaparse.ignore();
@@ -61,23 +105,51 @@ int main(){
 
       column++;
     }
+    if(column > lastcolumn){
+      lastcolumn = column;
+    }
     
     row++;   
   }
+  lastcolumn--;
 
   for(int i = 0; i < totalstates; i++){
     for(int j = 0; j < 3; j++){
-      for(int k = 0; k < states[i][j].size(); k++){
-        std::cout << states[i][j].at(k) << " ";
+      for(int k = 0; k < NFAstates[i][j].size(); k++){
+        std::cout << NFAstates[i][j].at(k) << " ";
       }
-      std::cout << "   ";
+      std::cout << "    ";
     }
     std::cout << "\n";
   }
 
+  // Get epsilon closure of start state
+  int start = initialstate-1;
+  vector<int> Iclose = epsilonClosure(NFAstates, start, lastcolumn);
+  std::cout << "E-Closure(I0) = {" << start+1;
+  for(int i = 1; i < Iclose.size(); i++){
+    std::cout << "," << Iclose.at(i);
+  }
+  std::cout << "}\n";
+
+  int currentstate = 0;
+  std::stack<vector<int>> inStack;
+  std::vector<vector<int>> outList;
+
+  inStack.push(Iclose);
+  while(!inStack.empty()){
+    std::cout << "Mark " << currentstate+1 << "\n";
+    std::vector<int> currentVector = inStack.top();
+    std::vector<int> tempVector = navigate(NFAstates, currentVector, 0);
+    inStack.pop();
+
+
+    
 
 
 
+
+  }
 
 
 
