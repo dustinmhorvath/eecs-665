bb00c2b624eb613b3a326e526efbc84341200b20
Dustin Horvath <dustinmhorvath@gmail.com>
2016-12-08 22:30:13 -0600

    Tidying up a bit.

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,378 +1,375 @@
-/*
- * Dustin Horvath
- * nfa2dfa
- *
- * EECS 665 Compilers
- * Project 1
- *
- * Takes an nfa table as input and produces a simplified dfa rule list and
- * state table as output. See file 'input.txt' for an example input format.
-
- *
- */
-
-
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <algorithm>
-#include <cctype>
+#include <functional> 
 #include <sstream>
+#include <iterator>
 #include <vector>
-#include <queue>
+#include <algorithm> 
+#include <cctype>
+#include <map>
+#include <set>
+#include <locale>
 #include <stack>
-#include <iomanip>
+using std::map;
+using std::ifstream;
+using std::stringstream;
+using std::string;
+using std::vector;
 
-using namespace std;
-
-#define MAXSYMBOLS 50
-#define MAXSIZE MAXSYMBOLS+1
-#define MAXDFASTATES 100
-
-
-vector<int> epsilonClosureHelper(std::vector<int> states[][MAXSIZE], int row, int epsilonCol){
-  vector<int> closure;
-  for(int i = 0; i < states[row][epsilonCol].size(); i++){
-    if(!states[row][epsilonCol].empty()){
-      closure.push_back(states[row][epsilonCol].at(i));
-      vector<int> recurseClose = epsilonClosureHelper(states, states[row][epsilonCol].at(i), epsilonCol);
-      closure.insert(closure.end(), recurseClose.begin(), recurseClose.end());
+struct nfa{
+  std::map<char, std::vector<int> > transitions;
+  std::set<int> e_closure(int this_state, std::vector<nfa> &states){
+    std::stack<int> stack1;
+    stack1.push(this_state);
+    std::set<int> resultSet;
+    resultSet.insert(this_state);
+    while(!stack1.empty()) {
+      int t = stack1.top();
+      stack1.pop();
+      for(int u : states[t].transitions['E']) {
+        if(resultSet.find(u) == resultSet.end()) {
+          resultSet.insert(u);
+          stack1.push(u);
+        }
+      }
     }
+    return resultSet;
   }
+};
 
-  std::sort(closure.begin(), closure.end());
-  return closure;
-}
-
-vector<int> epsilonClosure(std::vector<int> states[][MAXSIZE], vector<int> tocheck, int epsilonCol){
-  vector<int> closure = tocheck;
-  for(int vectorindex = 0; vectorindex < tocheck.size(); vectorindex++){
-    for(int i = 0; i < states[tocheck.at(vectorindex)][epsilonCol].size(); i++){
-      if(!states[tocheck.at(vectorindex)][epsilonCol].empty()){
-        closure.push_back(states[tocheck.at(vectorindex)][epsilonCol].at(i));
-        vector<int> recurseClose = epsilonClosureHelper(states, states[tocheck.at(vectorindex)][epsilonCol].at(i), epsilonCol);
-        closure.insert(closure.end(), recurseClose.begin(), recurseClose.end());
+struct dfa{   
+  dfa(std::set<int> nfaStates, std::vector<int> final_states) {
+    this->nfaStates = nfaStates;
+    this->final = false;
+    for(int i : final_states) {
+      for(int j : nfaStates) {
+        if(i == j) {
+          this->final = true;
+        }
       }
     }
   }
+  bool final;
+  std::map<char, int> transitions;
+  std::set<int> nfaStates;
 
-  std::sort(closure.begin(), closure.end());
-  return closure;
-}
+  bool operator==(const dfa &other) {
+    return std::equal(this->nfaStates.begin(), this->nfaStates.end(), other.nfaStates.begin());
+  }
+};
 
-vector<int> navigate(std::vector<int> states[][MAXSIZE], vector<int> tocheck, int col){
-  vector<int> destinations;
-  for(int vectorindex = 0; vectorindex < tocheck.size(); vectorindex++){
-    for(int i = 0; i < states[tocheck.at(vectorindex)][col].size(); i++){
-      if(!states[tocheck.at(vectorindex)][col].empty()){
-        destinations.push_back(states[tocheck.at(vectorindex)][col].at(i));
+std::set<int> move(dfa &s, char c, std::vector<nfa> &nfaStates) {
+  std::set<int> returnValues;
+  for(int i : s.nfaStates) {
+    nfa &temp = nfaStates[i];
+    std::map<char, vector<int> >::iterator it = temp.transitions.find(c);
+    if(it != temp.transitions.end()) {
+      for(int s : it->second) {
+        returnValues.insert(s);
       }
     }
   }
-
-  std::sort(destinations.begin(), destinations.end());
-  return destinations;
+  return returnValues;
 }
 
-bool compareIntVector(vector<int> a, vector<int> b){
-  bool equals = true;
-  if(a.size() != b.size()){
-    return false;
+void printNFA(vector<nfa> &s, vector<char> &a) {
+  std::cout<<"State";
+  for(int i = 0; i < a.size(); ++i) {
+    std::cout<<'\t'<<a[i];
   }
-  else{
-    for(int i = 0; i < a.size(); i++){
-      if(a.at(i) != b.at(i)){
-        equals = false;
+  std::cout<<"\n";
+  for(int i = 0; i < s.size(); ++i) {
+    std::cout<<i+1<<'\t';
+    for(char c: a) {
+      std::cout<<"{";
+      std::map<char, vector<int> >::iterator it = s[i].transitions.find(c);
+      if(it != s[i].transitions.end()) {
+        bool first = true;
+        for(int i: it->second) {
+          if(!first) {
+            std::cout<<',';
+          }
+          std::cout<<i+1;
+          first = false;
+        }
       }
+      std::cout<<"}"<<'\t';
     }
+    std::cout<<"\n";
   }
-
-  return equals;
 }
 
-bool checkVectorArray(std::vector<vector<int>> q, std::vector<int> v){
-  for(int i = 0; i < q.size(); i++){
-    if(compareIntVector(q.at(i), v)){
-      return true;
+void printTable(vector<dfa> &s, vector<char> &a) {
+  std::cout<<"State";
+  for(int i = 0; i < a.size() - 1; ++i) {
+    std::cout<<'\t'<<a[i];
+  }
+  std::cout<<"\n";
+  for(int i = 0; i < s.size(); ++i){
+    std::cout<<i+1<<'\t';
+    for(char c: a) {
+      if(c == 'E') {
+        continue;
+      }
+      std::cout<<"{";
+
+
+      std::map<char, int>::iterator it = s[i].transitions.find(c);
+      if(it != s[i].transitions.end()) {
+        std::cout<<it->second;
+      }
+      std::cout<<"}"<<'\t';
     }
 
+    std::cout<<"\n";
   }
-  return false;
-
-
 }
 
-// Returns the index of a particular vector in a list of vectors.
-int getMark(std::vector<vector<int>> q, std::vector<int> v){
-  for(int i = 0; i < q.size(); i++){
-    if(compareIntVector(q.at(i), v)){
-      return i;
+void printSet(std::set<int> s) {
+  std::cout<<"{";
+  bool first = true;
+  for( int i : s) {
+    if(!first) {
+      std::cout<<",";
     }
-
+    std::cout<<i+1;
+    first = false;
   }
-  return -1;
-
+  std::cout<<"}";
 }
 
-// Returns true if a particular value is in a vector<int>
-bool contains(vector<int> v, int state){
-  for(int i = 0; i < v.size(); i++){
-    if(v.at(i) == state){
-      return true;
+void printSet(std::vector<int> s) {
+  std::cout<<"{";
+  bool first = true;
+  for( int i : s) {
+    if(!first) {
+      std::cout<<",";
+    }
+    std::cout<<i+1;
+    first = false;
+  }
+  std::cout<<"}";
+}
+
+// trim from start
+static std::string &ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+        std::not1(std::ptr_fun<int, int>(std::isspace))));
+  return s;
+}
+
+// trim from end
+static std::string &rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+        std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+  return s;
+}
+
+// trim from both ends
+static std::string &trim(std::string &s) {
+  return ltrim(rtrim(s));
+}
+
+static std::string gensubstr(std::string s, std::string firstCharacter, std::string secondCharacter) {
+  string t_string = s;
+  std::size_t leftBracketIndex = t_string.find(firstCharacter);
+  std::size_t rightBracketIndex = t_string.find(secondCharacter);
+  t_string = t_string.substr(leftBracketIndex + 1, rightBracketIndex - leftBracketIndex - 1);
+  return trim(t_string);
+}
+
+static std::string gensubstr(std::string s, std::string firstCharacter) {
+  string t_string = s;
+  std::size_t leftBracketIndex = t_string.find(firstCharacter);
+  std::size_t rightBracketIndex = s.length();
+  t_string = t_string.substr(leftBracketIndex + 1, rightBracketIndex - leftBracketIndex - 1);
+  return trim(t_string);
+}
+
+static std::vector<int> splitstr(std::string s, char character) {
+  std::vector<int> iv;
+  trim(s);
+  stringstream ss(s);
+  while(ss.good()) {
+    string substring = "";
+    getline(ss, substring, character);
+    trim(substring);
+    if(substring == "") {
+    } else {
+      int i = stoi(substring) - 1;
+      iv.push_back(i);
     }
   }
-  return false;
+  return iv;
 }
 
-// Returns true if ANY value in a vector is in another vector.
-bool checkVectorElementInVector(vector<int> container, vector<int> in){
-  for(int i = 0; i < in.size(); i++){
-    if(contains(container, in.at(i))){
-      return true;
-    }
-  }
-  return false;
+int terminals(std::string const &str)
+{
+  std::stringstream stream(str);
+
+  return std::distance(std::istream_iterator<std::string>(stream), std::istream_iterator<std::string>());
 }
 
-int main( int argc, char *argv[] ){
-  if(argc<2){
-    std::cout << "Please provide an input file via the command line.\nExample: \"./nfa2dfa input.txt\"\n";
-    exit(0);
-  }
+int main(int argc, char* argv[]) {
+
+
+  std::cout << "Please provide an input file via stdin.\nExample: \"./nfa2dfa < input.txt\"\n\n";
 
   std::ifstream infile(argv[1]);
-  std::string line;
 
-  // Read in initial state
-  std::getline(infile, line);
-  unsigned first = line.find("{");
-  unsigned last = line.find("}");
-  int initialstate = std::stoi(line.substr(first + 1, last-first - 1));
 
-  // Read in final states
-  std::vector<int> finalstates;
-  std::getline(infile, line);
-  first = line.find('{');
-  last = line.find('}');
-  std::string tempString = line.substr(first + 1, last-first-1);
-  std::stringstream commaparse(tempString);
-  int finalstate;
-  while(commaparse >> finalstate){
-    finalstates.push_back(finalstate-1);
+  string line;
+  vector<string> npFile;
+  vector<string> pFile;
+  vector<char> symbols;
+  vector<nfa> states;
+  vector<dfa> dStates;
+  vector<int> final_states;
+  vector<int> d_final_states;
+  int number_of_terminals = 0;
+  int number_of_states = 0;
+  int initial_state = 0;
 
-    if (commaparse.peek() == ','){
-      commaparse.ignore();
-    }
+
+
+
+
+  while(std::getline(std::cin, line)) {
+    npFile.push_back(line);
   }
 
 
-  // Read in total states
-  std::getline(infile, line);
-  //int totalstates = std::stoi(gensubstr(line, ":"));
-  int totalstates = std::strtol(&line[line.find(':') + 1], nullptr, 10);
+  // Read in the initial state
+  string t_string = gensubstr(npFile[0], "{", "}");
+  initial_state = stoi(t_string);
 
-  // Consume header line
-  std::getline(infile, line);
+  // Read in the final state
+  t_string = gensubstr(npFile[1], "{", "}");
 
-  // Assuming we don't use more than the alphabet's worth of states, plus E
-  std::vector<int> NFAstates[totalstates][MAXSIZE];
+  // Read in number of final states
+  final_states = splitstr(t_string, ',');
 
-  // Parse table of inputs, tokenize on whitespace with stringstream
-  std::string word;
-  int row = 0;
-  int lastcolumn = 0;
-  while (std::getline(infile, line)){
-    std::stringstream ss(line);
-    // Consume row number
-    ss >> word;
+  //Get number of States
+  number_of_states = stoi(gensubstr(npFile[2], ":"));
 
-    // Tokenize states by parsing between commas
-    int column = 0;
-    while(ss >> word){
-      // Discard everything outside curly braces
-      first = word.find('{');
-      last = word.find('}');
-      std::string nocurlies = word.substr(first + 1, last-first-1);
-      std::stringstream commaparse(nocurlies);
-      int i;
-      while (commaparse >> i){
-        // NOTE decrementing indeces within the table by 1. This will convert
-        // the whole table to 0-index, so will need ++ for any cout's
-        NFAstates[row][column].push_back(i-1);
+  //Get number of terminals
+  t_string = npFile[3];
+  t_string = t_string.substr(5, t_string.length());
+  for(char c : t_string) {
+    if(c != ' ' && c != '\t') {
+      symbols.push_back(c);
+    }
+  }
+  number_of_terminals = terminals(t_string) - 1;
 
-        if (commaparse.peek() == ',')
-          commaparse.ignore();
+  // Resize states
+  states.resize(number_of_states);
+
+  for(int i = 4; i <= number_of_states + 3; ++i) {
+    std::string first = npFile[i];
+    std::string string1;
+    std::string string2;
+    std::stringstream ss(first);
+    ss >> string1;
+    size_t size1;
+    size_t size2;
+    for(int k = 0; k <= number_of_terminals; ++k){
+
+      ss >> string1;
+      size1 = string1.find("{");
+      size2 = string1.find("}");
+
+      string2 = string1.substr(size1+1, size2 - size1 - 1);
+      states[i-4].transitions[symbols[k]] = splitstr(string2, ',');
+
+    }
+  }   
+
+
+  std::cout<<"Initial State: {"<<initial_state<<"}\n";
+  std::cout<<"Final States: ";
+  printSet(final_states);
+  std::cout<<"\n";
+
+
+  std::cout<<"Total States: "<<number_of_states<<"\n";
+  printNFA(states,symbols);
+  std::cout<<"\n";
+
+
+  std::cout<<"E-closure(I0) = ";
+
+  std::set<int> resultSet;
+  resultSet = states[initial_state - 1].e_closure(initial_state - 1, states);
+  dfa initial(resultSet, final_states);
+  dStates.push_back(initial);
+  printSet(resultSet);
+  for(char c : symbols) {
+    move(initial, c, states);
+  }
+  std::cout<<" = 1\n\n";
+  for(int i = 0; i < dStates.size(); ++i) {
+    char c;
+    std::cout<<"Mark "<< i+1<<"\n";
+
+    for(char c: symbols) {
+      if(c == 'E') {
+        continue;
       }
+      resultSet = move(dStates[i], c, states);
+      if(resultSet.size() == 0) {
+        continue;
+      }
+      printSet(dStates[i].nfaStates);
+      std::cout<<" --"<<c<<"--> ";
+      printSet(resultSet);
 
+      std::set<int> ec;
+      for(int i : resultSet) {
+        std::set<int> tmp = states[i].e_closure(i, states);
+        ec.insert(tmp.begin(), tmp.end());
+      }
+      std::cout<<"\nE-closure";
+      printSet(resultSet);
+      std::cout << " = ";
+      printSet(ec);
 
-      column++;
-    }
-    if(column > lastcolumn){
-      lastcolumn = column;
-    }
-
-    row++;   
-  }
-  // Needs decremented, because it gets incremented one extra time.
-  lastcolumn--;
-
-  /* // Print the read-in values for testing
-     for(int i = 0; i < totalstates; i++){
-     for(int j = 0; j < 3; j++){
-     for(int k = 0; k < NFAstates[i][j].size(); k++){
-     std::cout << NFAstates[i][j].at(k) << " ";
-     }
-     std::cout << "    ";
-     }
-     std::cout << "\n";
-     }
-     */
-
-  // Get epsilon closure of start state
-  vector<int> start = {initialstate-1};
-  vector<int> Iclose = epsilonClosure(NFAstates, start, lastcolumn);
-  std::cout << "\nE-closure(I0) = {" << initialstate;
-  for(int i = 1; i < Iclose.size(); i++){
-    std::cout << "," << Iclose.at(i)+1;
-  }
-  std::cout << "} = 1\n";
-
-  int currentstate = 0;
-
-  // INQUEUE: stores the list of states that still need to be described, using
-  // a BFS mannerism.
-  std::queue<vector<int>> inQueue;
-  // OUTLIST: list of all the states that have been evaluated so far. This
-  // list must be consulted before pushing any new states onto inQueue, in
-  // order to ensure uniqueness
-  std::vector<vector<int>> outList;
-  // DFATABLE: stores the actual, new DFA transition table in its simplified
-  // form. lastcolumn is the number of symbols.
-  int DFAtable[MAXDFASTATES][lastcolumn];
-
-  // Holds the list of new final states for the DFA
-  std::vector<int> newFinals;
-
-  // Push initial closure onto our working queue
-  inQueue.push(Iclose);
-  outList.push_back(Iclose);
-
-  while(!inQueue.empty()){
-    // Pull a new working vector off the queue
-    std::vector<int> currentVector = inQueue.front();
-    inQueue.pop();
-
-    bool printedMark = false;
-    // Perform this block for every possible input symbol
-    for(int symbol = 0; symbol < lastcolumn; symbol++){
-      // Check where we can go from currentVector on 'symbol'
-      std::vector<int> tempVector = navigate(NFAstates, currentVector, symbol);
-
-      // Don't print anything if there are no destinations for a symbol
-      if(!tempVector.empty()){
-
-        if(!printedMark){
-          std::cout << "\nMark " << currentstate+1 << "\n";
-          printedMark = true;
-        }
-
-        // Print start
-        std::cout << "{";
-        if(currentVector.size() > 0){
-          std::cout << currentVector.at(0)+1;
-        }
-        for(int i = 1; i < currentVector.size(); i++){
-          std::cout << "," << currentVector.at(i)+1;
-        }
-        std::cout << "} --" << (char)((int)'a'+symbol) << "--> {";
-
-
-        // Print possible terminals on symbol
-        std::cout << tempVector.at(0)+1;
-        for(int i = 1; i < tempVector.size(); i++){
-          std::cout << "," << tempVector.at(i)+1;
-        }
-        std::cout << "}\n";
-
-        std::cout << "E-closure{";
-        std::cout << tempVector.at(0)+1;
-        for(int i = 1; i < tempVector.size(); i++){
-          std::cout << "," << tempVector.at(i)+1;
-        }
-        std::cout << "} = {";
-        tempVector = epsilonClosure(NFAstates, tempVector, lastcolumn);
-        if(!tempVector.empty()){
-          // Push the new state onto the queue if we haven't been there already
-          if(!checkVectorArray( outList, tempVector )){
-            inQueue.push(tempVector);
-            outList.push_back(tempVector);
-          }
-          std::cout << tempVector.at(0)+1;
-          for(int i = 1; i < tempVector.size(); i++){
-            std::cout << "," << tempVector.at(i)+1;
-          }
-          int mark = getMark(outList, tempVector)+1;
-          std::cout << "} = " << mark << "\n";
-
-          DFAtable[currentstate][symbol] = mark;
-
-
+      int flag = -1;
+      for(int k = 0; k < dStates.size(); ++k) {
+        //if(resultSet == dStates[k].nfaStates) {
+        if(std::equal(ec.begin(), ec.end(), dStates[k].nfaStates.begin())) {
+          flag = k;
         }
       }
-      else{
-        // Use zero so we don't have null values in table
-        DFAtable[currentstate][symbol] = 0;
+      if(flag == -1) {
+        //new state man
+        dfa newState(ec, final_states);
+        dStates.push_back(newState);
+        dStates[i].transitions[c] = dStates.size();
+        std::cout<<" = "<< dStates.size();
+
+        std::cout<<"\n";
+      } 
+      else {
+        std::cout<<" = " << flag+1;
+        dStates[i].transitions[c] = flag + 1;
+        std::cout<<"\n";
       }
-
-    }
-
-    // Check if our current state contains a final state
-    std::cout << "\n";
-    if(checkVectorElementInVector(finalstates, currentVector)){
-      newFinals.push_back(getMark(outList, currentVector));
-    }
-
-    currentstate++;
-  }
-
-  if(newFinals.empty()){
-    newFinals.push_back(0);
-  }
-
-  // Print nice table at end
-  std::cout << "\nInitial State: {" << initialstate << "}\n";
-  std::cout << "Final States: {" << newFinals.at(0)+1;
-  for(int i = 1; i < newFinals.size(); i++){
-    std::cout << "," << newFinals.at(i)+1;
-  }
-  std::cout << "}\n";
-
-  // Print column headers
-  int width = 8;
-  std::cout << std::setw(width) << left << "State";
-  for(int i = 0; i < lastcolumn; i++){
-    std::cout << std::setw(width) << left << (char)((int)'a' + i);
-  }
-  std::cout << "\n";
-
-  // Print the block table
-  for(int row = 0; row < outList.size(); row++){
-    std::cout << std::setw(width) << left << row+1;
-
-    for(int column = 0; column < lastcolumn; column++){
-      if(DFAtable[row][column] != 0){
-        std::stringstream concat;
-        concat << "{" << DFAtable[row][column] << "}";
-        std::cout << std::setw(width) << left << concat.str();
       }
-      else{
-        std::cout << std::setw(width) << left << "{}";
+      std::cout<<"\n";
+    }
+    for(int i = 0; i < dStates.size(); ++i) {
+      if(dStates[i].final == true) {
+        d_final_states.push_back(i);
       }
     }
+    std::cout<<"Initial State: {1}\n";
+    std::cout<<"Final States: ";
+    printSet(d_final_states);
+    std::cout<<"\n";
+    printTable(dStates,symbols);
 
 
-    std::cout << "\n";
+    return 0;
   }
-
-  std::cout << "\n";
-}
